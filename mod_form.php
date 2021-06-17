c89b73c26f86ee28615ea26e66da68833a207b3f
Yedidia Klein <yedidia@openapp.co.il>
2021-06-17 13:09:16 +0300

    Fixed path of vendor/autoload.php file

--- mod_form.php
+++ mod_form.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die;
 require_once ($CFG->dirroot.'/course/moodleform_mod.php');
 require_once($CFG->dirroot.'/mod/url/locallib.php');
 require_once($CFG->dirroot.'/mod/teams/lib.php');
-require_once($CFG->dirroot.'/vendor/autoload.php');
+require_once($CFG->dirroot.'/mod/teams/vendor/autoload.php');
 
 class mod_teams_mod_form extends moodleform_mod
 {
